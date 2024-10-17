ee929f4794a8c445807858908b5f8b9a8c2868fd
Syed Shazli <syed.abdu1818@gmail.com>
2024-10-16 21:33:43 -0400

    Feat: Added stuff??

--- functions/recursive.cpp
+++ functions/recursive.cpp
@@ -1,1 +1,18 @@
 //use chron jobs
+//chrontab(time, day, month, filepath): executes the script line by line by time specified
+// will make data collection easier
+// ex running chron tabs to parse reports to get data after each 3 days 
+// 'every week on tuesday at 3 am, email me the data'
+// server running, questionare to fill every sunday, output stored in file...
+//ping api of stock every afternoon (script gets price and chart of it to a file)
+// write script every day at 6 pm to grab output of 20 different stocks and sends email
+
+//write program in a way where it does something simple (write in procs)
+
+
+// data collection is my script. 'chron tab can run every minute'  
+// complicated decimal extraction, multiplication loop, division loop, etc. run 4 at a time, 1 at a time
+
+// build it simple and then optmise it later
+//.. check frame rate of graphic card, download 4k image how does it perform
+// 
